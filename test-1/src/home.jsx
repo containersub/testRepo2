bc939a92cf03edc4fce7c7e5d9b0532de1db8cdf
containersub <containersub@gmail.com>
2026-09-07 20:28:28 +0530

    update home

--- test-1/src/home.jsx
+++ test-1/src/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const home = () => {
   return (
     <div>
-        <h2>This is Home Page . Thanks for all</h2>
+        <h2>This is Home Page . in branch1</h2>
     </div>
   )
 }
